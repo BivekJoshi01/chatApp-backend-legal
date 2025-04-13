4d4d54dd7e7b97ef40fa2f5cc3b54b1de3534429
BivekJoshi01 <Bivek.joshi@legalremit.com>
2025-04-13 21:24:06 +0545

    added changes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controllers/auth/user.controller");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/auth/user.controller");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.route("/").post(registerUser);
-router.post("/login",authUser)
+router.post("/login", authUser);
+router.route("/").get(protect, allUsers);
 
 module.exports = router;
